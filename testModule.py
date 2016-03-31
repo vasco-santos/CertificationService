a916c2bcbb03b9dbfdd3a96fafd5c4baa233ff93
Vasco <vasco.santos@ua.pt>
2016-03-31 02:01:25 +0100

    Certificates Signatures Validation

--- testModule.py
+++ testModule.py
@@ -8,5 +8,18 @@ from certModule.CertSubject import CertificationSubject
 home = expanduser("~")
 
 ca = CertificationAuthority("ca_cert.pem", "ca_priv.pem", 'dioti')
+print("Certification Authority started...")
 
-cs = CertificationSubject("peer_cert.pub", "peer_priv.pem", "ca_cert.pem", 'dioti')
+if ca.validSelfSignedCertificate():
+    print("CA: Valid CA Auto Signed Certificate")
+
+    cs = CertificationSubject("peer_cert.pub", "peer_priv.pem", "ca_cert.pem", 'dioti')
+    print("Certification Subject Started...")
+
+    if cs.validCertificationAuthorityCertificate():
+        print ("CS: Valid CA Received Certification")
+
+        
+
+
+
